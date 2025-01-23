afea3fccf8d517d9f51c72ad0e1bca305bc612ef
Ji <overji1@sjtu.edu.cn>
2025-01-23 14:37:39 +0800

    fixed some bugs

--- plugins/roll_dice.py
+++ plugins/roll_dice.py
@@ -4,10 +4,10 @@ import random
 def interface_roll_dice(args):
     start = 1
     end = 6
-    if(args.size() == 2):
+    if(len(args) == 2):
         start = int(args[0])
         end = int(args[1])
-    elif(args.size() == 1):
+    elif(len(args) == 1):
         end = int(args[0])
     else:
         pass
