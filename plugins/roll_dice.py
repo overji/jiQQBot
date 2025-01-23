2750eb793ec43057280c2bd4f1a6815dbcb85096
Ji <overji1@sjtu.edu.cn>
2025-01-23 14:36:04 +0800

    掷骰子功能更好用了

--- plugins/roll_dice.py
+++ plugins/roll_dice.py
@@ -2,4 +2,13 @@ import random
 
 
 def interface_roll_dice(args):
-    return f"掷骰子的结果是:{str(random.randint(1, 6))}"
+    start = 1
+    end = 6
+    if(args.size() == 2):
+        start = int(args[0])
+        end = int(args[1])
+    elif(args.size() == 1):
+        end = int(args[0])
+    else:
+        pass
+    return f"掷骰子的结果是:{str(random.randint(start,end))}"
